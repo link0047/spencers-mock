befde21d4e4d00a5a4bfa0a579e0d12d615a7483
link0047 <link0047@gmail.com>
2023-06-15 09:39:25 -0400

    add security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,9 +3,20 @@ import { handleHooks } from "@lucia-auth/sveltekit";
 import type { Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 
+const securityHeaders = new Map([
+  // ["cross-origin-embedder-policy", "require-corp"],
+  ["cross-origin-opener-policy", "same-origin"],
+  ["cross-origin-resource-policy", "same-origin"],
+  ["referrer-policy", "no-referrer"],
+  ["x-content-type-options", "nosniff"],
+  ["x-frame-options", "SAMEORIGIN"],
+]);
+
 export const customHandle: Handle = async ({ resolve, event }) => {
   event.locals.ua = event.request.headers.get('user-agent');
   const response = await resolve(event);
+
+  securityHeaders.forEach((value, header) => response.headers.set(header, value));
 
   return response;
 }
