6a5f956fce8ea4b2040aa03f6c2f29e080a8e520
link0047 <link0047@gmail.com>
2024-03-07 12:29:40 -0500

    test

--- src/lib/components/productGallery/ProductGallery.svelte
+++ src/lib/components/productGallery/ProductGallery.svelte
@@ -2,15 +2,22 @@
 	import Carousel from "$lib/components/carousel/Carousel.svelte";
 	import CarouselSlide from "$lib/components/carousel/CarouselSlide.svelte";
 	import ZoomViewer from "$lib/components/zoomviewer/ZoomViewer.svelte";
+  import { onMount } from "svelte";
+
 	export let images = [];
 	let carousel;
+	let isMobile;
 
 	let selectedImageIndex = 0;
 	
 	function changeProduct(index) {
 		carousel.gotoSlide(index);
 		selectedImageIndex = index;
-	}	
+	}
+	
+	onMount(() => {
+		isMobile = window.matchMedia("(max-width: 560px)");
+	})
 </script>
 
 <div class="product-gallery">
@@ -26,7 +33,6 @@
 			</button>
 		{/each}
 	</div>
-	
 	<Carousel bind:slideIndex={selectedImageIndex} bind:this={carousel} slidesPerView={1} displayIndicator={true}>
 		{#each images as { src, detailedSrc }}
 			<CarouselSlide>
@@ -41,9 +47,8 @@
 		display: grid;
 		grid-template-columns: 62px 640px;
 		gap: .5rem;
-		width: 320px;
 	}
-	
+
 	.product-gallery__thumbnails {
 		display: flex;
 		flex-flow: column;
@@ -67,5 +72,14 @@
 
 	[data-state="selected"].product-gallery__btn {
 		border-color: #000;
+	}
+
+	@media(max-width: 560px) {
+		.product-gallery {
+			grid-template-columns: 1fr;
+		}
+		.product-gallery__thumbnails {
+			display: none;
+		}
 	}
 </style>
