79aa8d5206c4405cbe0f3103184f59ebf2c533bf
link0047 <link0047@gmail.com>
2024-03-14 11:18:32 -0400

    update carousel - add hide next and prev functionality

--- src/lib/components/carousel/Carousel.svelte
+++ src/lib/components/carousel/Carousel.svelte
@@ -436,16 +436,20 @@
 	</div>
 	<div class="carousel__viewport">
 		<div class="carousel__controls">
-			<button class="previous" aria-controls={trackId} aria-label="Previous Slide" on:click={prevSlide}>
-				<svg class="icon" role="presentation" focusable="false" viewBox="0 0 24 24">
-					<path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
-				</svg>
-			</button>
-			<button class="next" aria-controls={trackId} aria-label="Next Slide" on:click={nextSlide}>
-				<svg class="icon" role="presentation" focusable="false" viewBox="0 0 24 24">
-					<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-				</svg>
-			</button>
+			{#if slideIndex > 0}
+				<button class="previous" aria-controls={trackId} aria-label="Previous Slide" on:click={prevSlide}>
+					<svg class="icon" role="presentation" focusable="false" viewBox="0 0 24 24">
+						<path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+					</svg>
+				</button>
+			{/if}
+			{#if slideIndex < slideCount - 1}
+				<button class="next" aria-controls={trackId} aria-label="Next Slide" on:click={nextSlide}>
+					<svg class="icon" role="presentation" focusable="false" viewBox="0 0 24 24">
+						<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+					</svg>
+				</button>
+			{/if}
 			<div class="carousel__indicator">
 				{#if displayIndicator}
 					<IndicatorDots index={slideIndex} count={slideCount} />
