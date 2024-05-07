bffbef72eed2684a726ac1c793a86051c0534166
link0047 <link0047@gmail.com>
2024-05-07 16:09:50 -0400

    add aria label to dialog dismiss button

--- src/lib/components/dialog/DialogDismiss.svelte
+++ src/lib/components/dialog/DialogDismiss.svelte
@@ -3,10 +3,12 @@
   import Icon from "$lib/components/icon";
   import { getContext } from "svelte";
 
+  export let label = "close";
+
   const close = getContext<() => void>("close");
 </script>
 
-<Button on:click={close} variant="icon">
+<Button on:click={close} variant="icon" aria-label={label}>
   <Icon>
     <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41Z"/>
   </Icon>
